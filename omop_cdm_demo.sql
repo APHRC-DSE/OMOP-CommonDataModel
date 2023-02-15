d90d4622c76cccd4103bec285e72f8bd446ccf7c
Michael  Ochola <mochola@AD.APHRC.ORG>
2023-02-16 00:29:47 +0300

    sample omop-cdm queries for bigquery synthetic data

---
+++ omop_cdm_demo.sql
@@ -0,0 +1,26 @@
+-- A Demo to showcase various tables on omop cdm using synthetic data
+
+
+/* Concepts are referred by concept_id. All details are in CONCEPT table */
+
+select * from bigquery-public-data.cms_synthetic_patient_data_omop.concept as c
+where c.concept_id = 313217;
+
+select * from bigquery-public-data.cms_synthetic_patient_data_omop.concept as c
+where c.concept_code = '49436004';
+
+
+select * from bigquery-public-data.cms_synthetic_patient_data_omop.concept as c
+where c.concept_name = 'Atrial fibrillation';
+
+
+/* Various Vocabularies */
+
+select DISTINCT(vocabulary_id) from bigquery-public-data.cms_synthetic_patient_data_omop.vocabulary as v
+
+/* A unique count of patients in person table */
+
+select COUNT(*) from bigquery-public-data.cms_synthetic_patient_data_omop.person as p
+
+
+
